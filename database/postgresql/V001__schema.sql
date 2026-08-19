fb9d9d028c0dda2e9a4ac8e78f4e1c72ddd56e54
Jonathan Quintana Cueva <56851031+jonathanq1985@users.noreply.github.com>
2026-08-19 14:19:00 -0500

    Update V001__schema.sql
    feat: creación tabla empresa

--- database/postgresql/V001__schema.sql
+++ database/postgresql/V001__schema.sql
@@ -5,3 +5,20 @@ CREATE SCHEMA IF NOT EXISTS productos;
 CREATE SCHEMA IF NOT EXISTS inventario;
 CREATE SCHEMA IF NOT EXISTS ventas;
 CREATE SCHEMA IF NOT EXISTS auditoria;
+
+
+CREATE TABLE configuracion.empresa (
+    id_empresa BIGSERIAL PRIMARY KEY,
+    codigo VARCHAR(50) NOT NULL UNIQUE,
+    razon_social VARCHAR(200) NOT NULL,
+    nombre_comercial VARCHAR(200),
+    ruc VARCHAR(11) UNIQUE,
+    direccion VARCHAR(300),
+    telefono VARCHAR(20),
+    correo VARCHAR(150),
+    moneda_principal VARCHAR(5) DEFAULT 'PEN',
+    estado BOOLEAN DEFAULT TRUE,
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_actualizacion TIMESTAMP
+);
+
