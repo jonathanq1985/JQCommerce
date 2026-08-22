33ad9a871cfbcae09d0b8b957cdd8314ec926f46
jonathan Quintana <jonathanq1985@gmail.com>
2026-08-22 16:14:47 -0500

    feat: validación usuario desde base de datos

--- backend/ms-auth/src/main/java/com/jqdigitalsolutions/jqcommerce/auth/service/AuthService.java
+++ backend/ms-auth/src/main/java/com/jqdigitalsolutions/jqcommerce/auth/service/AuthService.java
@@ -2,29 +2,43 @@ package com.jqdigitalsolutions.jqcommerce.auth.service;
 
 import com.jqdigitalsolutions.jqcommerce.auth.dto.LoginRequest;
 import com.jqdigitalsolutions.jqcommerce.auth.dto.LoginResponse;
+import com.jqdigitalsolutions.jqcommerce.auth.entity.Usuario;
+import com.jqdigitalsolutions.jqcommerce.auth.repository.UsuarioRepository;
 import com.jqdigitalsolutions.jqcommerce.auth.security.JwtService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
 
+    private final UsuarioRepository usuarioRepository;
     private final JwtService jwtService;
 
-    public AuthService(JwtService jwtService) {
+    public AuthService(
+            UsuarioRepository usuarioRepository,
+            JwtService jwtService) {
+
+        this.usuarioRepository = usuarioRepository;
         this.jwtService = jwtService;
     }
 
     public LoginResponse login(LoginRequest request) {
 
+        Usuario usuario = usuarioRepository
+                .findByUsername(request.username())
+                .orElseThrow(() ->
+                        new RuntimeException(
+                                "Usuario no encontrado"
+                        )
+                );
+
         String token =
                 jwtService.generateToken(
-                        request.username()
+                        usuario.getUsername()
                 );
 
         return new LoginResponse(
                 token,
                 "Bearer"
         );
-
     }
 }
